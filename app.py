4bb97ccccf2844438ecd5ccfd6f3f9d5e6328c10
Saruultugs <saruul_14@yahoo.com>
2018-10-24 04:34:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,18 +84,13 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
-def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-    try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
+def log(msg, *args, **kwargs):
+    if type(msg) is dict:
+        msg = json.dumps(msg)
         else:
-            #msg = unicode(msg).format(*args, **kwargs)
             msg = msg.format(*args, **kwargs)
-        print u"{}: {}".format(datetime.now(), msg)
-    except UnicodeEncodeError:
-        pass  # squash logging errors in case of non-ascii text
-    sys.stdout.flush()
-
+            print u"{}: {}".format(datetime.now(), msg)
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
