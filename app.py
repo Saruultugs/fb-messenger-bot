5c08aed5056b256ccfa87ec23a6a303959201947
Saruultugs <saruul_14@yahoo.com>
2018-10-25 05:56:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,11 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    
+                    if message_text != messaging_event["message"]["text"]:
+                        return 'Kod buruu bna!'
+                    else:
+                        message_text = messaging_event["message"]["text"]  # the message's text
                     
                     #if 'btcb' != message_text:
                         #send_message(sender_id, "Ok")
