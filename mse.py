9173670b0d64402d57e781c62762c2ac96329a54
Saruultugs <saruul_14@yahoo.com>
2018-10-26 00:42:02 +0800

    Create mse.py

---
+++ mse.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_adu():
+
+	page = requests.get("http://mse.mn/mn/company/231")
+	soup = BeautifulSoup(page.content, 'html.parser')
+	c = soup.find('div', class_='col-lg-6 col-md-6')
+	c_a = soup.find_all('b')
+	return 'APU:' + str(c_a[2].get_text()) + '₮'
+
+print(get_adu())
